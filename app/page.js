ebc9b2588be99fd87048b7b4ce37545bbd8ff093
Johan Piyabordin <kaiju5678@gmail.com>
2025-11-09 13:04:16 +0700

    Edit page.js

--- app/page.js
+++ app/page.js
@@ -1,17 +1,57 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import FormComponent from "@/components/FormComponent";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter();
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const getAllProducts = async () => {
+
+    try {
+      const res = await fetch("https://dummyjson.com/products")
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const allProduct = await res.json();
+      setProducts(allProduct.products);
+      setFilteredProducts(allProduct.products);
+
+    } catch (error) {
+      setProducts([]);
+      setFilteredProducts([]);
+      console.error("Error fetching products:", error);
+    }
+
+  }
+  console.log("--[products]--", products);
+  console.log("--[filteredProducts]--", filteredProducts);
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <div>
       <Header />
       <FormComponent />
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 m-10">
-        {/* Code */}
+        {filteredProducts.map((item) => (
+          <div key={item.id} className="flex flex-col items-center border border-gray-300 rounded-3xl hover:shadow-lg cursor-pointer"
+          onClick={() => router.push(`/products/${item.id}`)}>
+            <img src={item.thumbnail} alt={item.title} />
+            <h2>{item.name}</h2>
+            <p className="text-[16px] font-bold">{item.title}</p>
+            <p className="text-[16px] ">${item.price}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,9 @@ import { AiOutlineProduct } from "react-icons/ai";
 
 export default function Header() {
   return (
-    <div className="flex items-center justify-center gap-2 h-[124px]">
+    <div className="flex items-center justify-center gap-3 h-[124px] bg-gradient-to-r from-pink-500 via-orange-400 to-yellow-400 text-white rounded-2xl shadow-lg">
       <AiOutlineProduct fontSize="40px" />
-      <span className="text-4xl font-bold text-center">Product</span>
+      <span className="text-4xl font-bold text-center">กินเข่ากันพี่น้อง แซ่บๆ</span>
     </div>
   );
 }
